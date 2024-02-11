df92f14b4e1c5a90af4c0455dccc22891d3457c0
Uri Shaked <uri@urishaked.com>
2024-02-11 21:53:22 +0200

    style: format RTL verilog code

--- test/tb.v
+++ test/tb.v
@@ -1,5 +1,5 @@
 `default_nettype none
-`timescale 1ns/1ps
+`timescale 1ns / 1ps
 
 /*
 this testbench just instantiates the module and makes some convenient wires
@@ -16,28 +16,28 @@ module tb (
     input [7:0] data_in,
     output [7:0] bidirectional_is_output,
     output [7:0] data_out
-   );
+);
 
-    // this part dumps the trace to a vcd file that can be viewed with GTKWave
-    initial begin
-        $dumpfile ("tb.vcd");
-        $dumpvars (0, tb);
-        #1;
-    end
+  // this part dumps the trace to a vcd file that can be viewed with GTKWave
+  initial begin
+    $dumpfile("tb.vcd");
+    $dumpvars(0, tb);
+    #1;
+  end
 
-    // instantiate the DUT
-    tt_um_urish_dffram dffram(
-    `ifdef GL_TEST
-        .VPWR( 1'b1),
-        .VGND( 1'b0),
-    `endif
-        .ui_in  ({we, addr}),
-        .uo_out (data_out),
-        .uio_in (data_in),
-        .uio_oe (bidirectional_is_output),
-        .ena(ena),
-        .clk(clk),
-        .rst_n(rst_n)
-    );
+  // instantiate the DUT
+  tt_um_urish_dffram dffram (
+`ifdef GL_TEST
+      .VPWR(1'b1),
+      .VGND(1'b0),
+`endif
+      .ui_in({we, addr}),
+      .uo_out(data_out),
+      .uio_in(data_in),
+      .uio_oe(bidirectional_is_output),
+      .ena(ena),
+      .clk(clk),
+      .rst_n(rst_n)
+  );
 
 endmodule
